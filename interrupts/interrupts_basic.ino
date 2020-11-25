917b6573f176be4efb920986acc5fe7c8cd98785
Gültekin Bulgurcu <bulgurcu32@gmail.com>
2020-11-26 00:59:27 +0300

    Update interrupts_basic.ino

--- interrupts/interrupts_basic.ino
+++ interrupts/interrupts_basic.ino
@@ -1,3 +1,15 @@
+//attachInterrupt(digitalPinToInterrupt(A), B, C);
+  //A = Input Tanımı
+  //B = Kesme meydana gelince çalışacak olan komut dizisi
+  //C = Kesmenin meydana geleceği koşul
+
+//LOW = Düşük olduğunda
+//HIGH = Yüksek olduğunda
+//CHANGE = Durum her değiştiğinde kesmeyi tetiklemek için. Örneğin, HIGH'dan LOW'a veya LOW'dan HIGH'a gittiğinde
+//FALLING = HIGH'dan LOW'a geçtiğinde.
+//RISING = LOW'dan HIGH'a geçtiğinde tetiklemek için
+
+
 int buton = 14; //Pin tanımlaması
 
 void setup(){
