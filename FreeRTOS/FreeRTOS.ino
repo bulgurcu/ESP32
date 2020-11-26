8ef3e4ce8a2d59146c2b2fd1fa94af45707c218e
Gültekin Bulgurcu <bulgurcu32@gmail.com>
2020-11-26 05:40:19 +0300

    Update FreeRTOS.ino

--- FreeRTOS/FreeRTOS.ino
+++ FreeRTOS/FreeRTOS.ino
@@ -3,6 +3,7 @@ void setup() {
  
   xTaskCreate(GorevBir,"GorevBir",10000,NULL,1,NULL); //GorevBir Task tanımı
   xTaskCreate(Goreviki,"Goreviki",10000,NULL,1,NULL); //Goreviki Task tanımı
+  xTaskCreate(GorevUc,"GorevUc",10000,NULL,1,NULL); //Goreviki Task tanımı  
 }
  
 void loop() {
@@ -10,18 +11,28 @@ void loop() {
  
 void GorevBir( void * parameter ){ //GorevBir komut satırı
     for( int i = 0;i<10;i++ ){
-      Serial.println("Hello from task 1");
+      Serial.println("Görev 1");
       delay(500);
     }
-    Serial.println("Ending task 1");
+    Serial.println("Görev 1 Bitti");
     vTaskDelete( NULL );
 }
  
 void Goreviki( void * parameter){ //Goreviki komut satırı
     for( int i = 0;i<10;i++ ){
-        Serial.println("Hello from task 2");
+        Serial.println("Görev 2");
         delay(1000);
     }
-    Serial.println("Ending task 2");
+    Serial.println("Görev 2 Bitti");
     vTaskDelete( NULL );
+}
+
+void GorevUc( void * parameter){ //GorevUc sonsuz görev komut satırı
+  while(1){ // while komutu döngüyü sonsuza kadar sürdürüyor
+    for( int i = 0;i<10;i++ ){
+        Serial.println("Görev 3 ");
+        delay(1000);
+    }
+    Serial.println("Görev 3 Bitti");
+  }    
 }
